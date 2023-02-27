1271357414cb5ae256c9ebf20e7f0c22c3b1e357
Dolnys <thedolnys@gmail.com>
2023-02-27 20:16:19 +0100

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:woc/firebase_options.dart';
 import 'package:woc/screens/categories_coctails_screen.dart';
 import 'package:woc/screens/coctail_detail_screen.dart';
 import 'package:woc/screens/filters_screen.dart';
 import 'package:woc/screens/tabs_screen.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
